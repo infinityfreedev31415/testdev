d6e8ca75b93c260cd2e4178607e31759f71d699c
TEST <infinityfree.dev31415@gmail.com>
2025-01-12 21:42:19 -0500

    updates

---
+++ common.js
@@ -0,0 +1,4 @@
+document.addEventListener("load", function() {
+    let htmlstr = '<pre>[ Object Browser | <a href="./editor/index.html">Editor</a> | Bookmarks ]</pre>';
+    document.getElementById("headernav").innerHTML=htmlstr;
+});
